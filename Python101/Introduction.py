d5005ac1df12e79dd50f9c1a8f1ec3421666f07c
Amani Mkumbukwa <amanimk145@gmail.com>
2026-08-11 11:51:15 +0300

    Third commit

---
+++ Python101/Introduction.py
@@ -0,0 +1,8 @@
+name= "Amani"
+student_course= "Computer Engineering"
+age= int(input("Enter your age: "))
+print(f"Name: {name}")
+print(f"Course: {student_course}")
+print(f"Age: {age}")
+print(f"Hello, {name}! Welcome to the {student_course} program.")
+print(f"Next year, you will be {age + 1} years old.")
